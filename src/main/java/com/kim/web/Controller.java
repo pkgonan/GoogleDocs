206e8f6f0cb6c1b2e6c69b2b2ba0d7a56ebc47ab
pkgonan <pkgonan1@naver.com>
2016-12-13 03:07:24 +0900

    Main Controller

---
+++ src/main/java/com/kim/web/Controller.java
@@ -0,0 +1,14 @@
+package com.kim.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    ModelAndView home() {
+        return new ModelAndView("index");
+    }
+}
