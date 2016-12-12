39bfd96e1540acd5b73d1da36505c08ddb99535c
pkgonan <pkgonan1@naver.com>
2016-12-13 02:59:14 +0900

    Project Creation

---
+++ src/main/java/com/kim/GoogledocsApplication.java
@@ -0,0 +1,12 @@
+package com.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledocsApplication.class, args);
+	}
+}
